29d997099f6c533c63ecfcc0bf63f2c05b540aa5
rhit-perezbe <perezbe@rose-hulman.edu>
2023-10-09 03:26:10 +0000

    Added firmware queue code

---
+++ NanoPi Code/Firmware/hampod_queue.h
@@ -0,0 +1,22 @@
+#ifndef HAMPOD_QUEUE
+#define HAMPOD_QUEUE
+#include "hampod_firm_packet.h"
+
+typedef struct Node {
+    Inst_packet* packet;
+    struct Node* next;
+} Node;
+
+typedef struct Packet_queue {
+    Node* head;
+    Node* tail;
+} Packet_queue;
+
+Packet_queue* create_packet_queue();
+void enqueue(Packet_queue* queue, Inst_packet *packet);
+Inst_packet* dequeue(Packet_queue* queue);
+void destroy_queue(Packet_queue* queue);
+
+#include "hampod_queue.c"
+
+#endif
