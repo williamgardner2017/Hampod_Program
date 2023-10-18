9bf76cc8d6642298bf2d6353a97136b10180c261
Perez <perezbe@rose-hulman.edu>
2023-10-18 15:24:12 -0400

    Keypad pipe fixes

--- NanoPi Code/Firmware/firmware.c
+++ NanoPi Code/Firmware/firmware.c
@@ -29,7 +29,7 @@ void sigint_handler(int signum);
 
 int main(){
 #ifdef DEBUG
-    printf("\033[0;32mHampod Firmware Version 0.51\n");
+    printf("\033[0;32mHampod Firmware Version 0.52\n");
     printf("\033[0;31mDEBUG BUILD \033[1;33m\n");
     printf("Clearing Pipes\n");
     system("./pipe_remover.sh");
@@ -93,10 +93,6 @@ int main(){
         exit(1);
     }
     int keypad_in_pipe_fd = open(KEYPAD_IN, O_WRONLY);
-    if(keypad_in_pipe_fd == -1){
-        perror("open");
-        exit(1);
-    }
 
 #ifdef DEBUG
     printf("Keypad_i created\n");
@@ -108,6 +104,12 @@ int main(){
         exit(1);
     }
     int keypad_out_pipe_fd = open(KEYPAD_OUT, O_RDONLY);
+    
+    if(keypad_in_pipe_fd == -1){
+        perror("open");
+        exit(1);
+    }
+    
     if(keypad_out_pipe_fd == -1){
         perror("open");
         exit(1);
