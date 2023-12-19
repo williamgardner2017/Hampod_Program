ba87734c9b5cbe6169197b42ddda4d3c2723df14
williamgardner2017 <william.gardner2017@outlook.com>
2023-12-19 17:00:02 -0500

    Dummy code implemented

--- NanoPi Code/Software/Radio.c
+++ NanoPi Code/Software/Radio.c
@@ -1,3 +1,5 @@
+#define SERIAL_PORT "/dev/ttyUSB0"
+
 Radio* loadUpRadioUsingData(char* make, int model, int port, Mode* defaultMode, rig_model_t myrig_model){
     Radio* newRadio = malloc(sizeof(Radio));
     newRadio->make = make;
@@ -6,6 +8,8 @@ Radio* loadUpRadioUsingData(char* make, int model, int port, Mode* defaultMode, 
     newRadio->currentMode = defaultMode;
     newRadio->myrig_model = myrig_model; 
     newRadio->my_rig = rig_init(myrig_model);
+    rig_set_conf(newRadio->my_rig, rig_token_lookup(newRadio->my_rig, "rig_pathname"), SERIAL_PORT);
+    rig_open(newRadio->my_rig); 
     return newRadio;
 }
 
@@ -30,7 +34,7 @@ void setRadioMode(Radio* thisRadio, Mode* modeToSetTo){
 void* runRadioCommand(Radio* thisRadio, KeyPress* keyInput){
     PRINTFLEVEL1("running mode of %s\n",thisRadio->currentMode->modeDetails->modeName);
     void* results;
-    results = thisRadio->currentMode->modeInput(keyInput,0);
+    results = thisRadio->currentMode->modeInput(keyInput,thisRadio->my_rig);
     return results;
 }
 
