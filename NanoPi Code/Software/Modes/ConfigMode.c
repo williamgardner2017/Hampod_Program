69b5998c28f38c815f80d1178181aa96f8ee1879
rhit-SlaterE <slatere@rose-hulman.edu>
2023-12-27 16:16:26 -0500

    added save and cancle, may need to remap buttons tho

--- NanoPi Code/Software/Modes/ConfigMode.c
+++ NanoPi Code/Software/Modes/ConfigMode.c
@@ -1,10 +1,11 @@
 char** configNames;
 int currentConfig = 0;
+double* oldValues;
 /**
  * 4,6 go forward or backwards in a config 
  * 2,8 go up and down for the configs
- * _, save changes
- * _, cancel changes
+ * 7, save changes
+ * 9, cancel changes
  * 5 run the config spesific command 
 */
 void* configCommandRelay(KeyPress* keyInput, int radioDetails){
@@ -15,7 +16,7 @@ void* configCommandRelay(KeyPress* keyInput, int radioDetails){
         if(currentConfig < 0){
             currentConfig = getLengthOfConfigs();
         }
-        PRINTFLEVEL1("SOFTWARE: switched configs to %s", configNames[currentConfig]);
+        PRINTFLEVEL1("SOFTWARE: switched configs to %s\n", configNames[currentConfig]);
         //say the name
         break;
     
@@ -24,19 +25,35 @@ void* configCommandRelay(KeyPress* keyInput, int radioDetails){
         if(currentConfig >= getLengthOfConfigs()){
             currentConfig = 0;
         }
-        PRINTFLEVEL1("SOFTWARE: switched configs to %s", configNames[currentConfig]);
+        PRINTFLEVEL1("SOFTWARE: switched configs to %s\n", configNames[currentConfig]);
         //say the name
         break;
     case '2':
         char* output = updateConfig(configNames[currentConfig], false);
-        PRINTFLEVEL1("SOFTWARE: Set config %s to %s", configNames[currentConfig], output);
+        PRINTFLEVEL1("SOFTWARE: Set config %s to %s\n", configNames[currentConfig], output);
         free(output);
         break;
     case '8':
         char* output = updateConfig(configNames[currentConfig], true);
-        PRINTFLEVEL1("SOFTWARE: Set config %s to %s", configNames[currentConfig], output);
+        PRINTFLEVEL1("SOFTWARE: Set config %s to %s\n", configNames[currentConfig], output);
         free(output);
         break;
+
+    case '5':
+        PRINTFLEVEL1("SOFTWARE: Running function related to config%s\n", configNames[currentConfig]);
+        getConfigByName(configNames[currentConfig])->configFuntion(NULL);
+        break;
+
+    case '9':
+        PRINTFLEVEL1("SOFTWARE: Saving changes to configs\n");
+        oldValues = getListOfCurrentValues();
+        break;
+
+    case 'y':
+        PRINTFLEVEL1("SOFTWARE: canceling changes to configs\n");
+        setListOfcurrentValues(oldValues);
+        break;
+
     default:
         break;
     }
@@ -72,5 +89,6 @@ Mode* ConfigLoad(){
 
     newMode->modeDetails = newData;
     configKeys = getListOfConfigNames();
+    oldValues = getListOfCurrentValues();
     return newMode;
 }
