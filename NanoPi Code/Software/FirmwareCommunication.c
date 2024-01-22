ee2eaad8f5d808c7b320e4b47fefe1d582f48ea3
rhit-SlaterE <slatere@rose-hulman.edu>
2024-01-22 15:55:47 -0500

    tring a more direct path rather then using ~

--- NanoPi Code/Software/FirmwareCommunication.c
+++ NanoPi Code/Software/FirmwareCommunication.c
@@ -303,7 +303,7 @@ char* sendSpeakerOutput(char* text){
 
 
 void setupAudioHashMap(){
-    char* softwarePath = "~/Hampod_Program/NanoPi Code/Firmware/pregen_audio";
+    char* softwarePath = "/home/pi/Hampod_Program/NanoPi Code/Firmware/pregen_audio";
     PRINTFLEVEL2("SOFTWARE:Creating the hashmap\n");
     audioHashMap = createHashMap(audioHash,audioCompare);
     struct dirent *de; 
