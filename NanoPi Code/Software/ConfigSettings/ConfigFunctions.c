b339ada76802529b1abfdbc79b11b6eb2c64c752
rhit-SlaterE <slatere@rose-hulman.edu>
2024-01-24 16:39:05 -0500

    Seeing if I am in the correct directory

--- NanoPi Code/Software/ConfigSettings/ConfigFunctions.c
+++ NanoPi Code/Software/ConfigSettings/ConfigFunctions.c
@@ -9,6 +9,13 @@ void populateConfigFunctions(){
 }
 
 void saveToFile(int fileNumber){
+    char cwd[1024];
+if (getcwd(cwd, sizeof(cwd)) != NULL) {
+    printf("Current working directory: %s\n", cwd);
+} else {
+    perror("getcwd() error");
+    return 1;
+}
     char fileName[40];
     sprintf(fileName, "SaveFiles/saveNumber%i.txt",fileNumber);
     FILE *fp = fopen(fileName, "w+");
