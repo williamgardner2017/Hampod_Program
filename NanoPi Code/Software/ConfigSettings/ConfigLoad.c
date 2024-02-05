c5eb2d8e5eecdcb5d81acd33dcde44d0640f0dc9
rhit-SlaterE <slatere@rose-hulman.edu>
2024-02-05 16:20:19 -0500

    trying to get the indexes to be correct

--- NanoPi Code/Software/ConfigSettings/ConfigLoad.c
+++ NanoPi Code/Software/ConfigSettings/ConfigLoad.c
@@ -61,9 +61,9 @@ void loadUpFromSave(int saveFileNumber){
         char* sPort = strchr(sModel, ':') + 1;
         char* sRigModel = strchr(sPort, ':') + 1;
         PRINTFLEVEL2("SOFTWARE: Created RAW strings %s, %s, %s, %s to load in\n",sMake,sModel,sPort,sRigModel);
-        sMake = customSubString(sMake,0,sMake-sModel);
-        sModel = customSubString(sModel,0,sModel-sPort);
-        sPort = customSubString(sPort,0,sPort-sRigModel);
+        sMake = customSubString(sMake,0,(int) (sMake-sModel));
+        sModel = customSubString(sModel,0,(int) (sModel-sPort));
+        sPort = customSubString(sPort,0,(int) (sPort-sRigModel));
         PRINTFLEVEL2("SOFTWARE: Created filtered strings %s, %s, %s, %s to load in\n",sMake,sModel,sPort,sRigModel);
         int iModel = atoi(sModel);
         int iPort = atoi(sPort);
