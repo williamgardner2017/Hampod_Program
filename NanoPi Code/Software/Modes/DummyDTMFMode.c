f9f1060643c70a5944277ecdc84f51c768c9ee14
williamgardner2017 <william.gardner2017@outlook.com>
2024-02-15 19:22:01 -0500

    Update DummyDTMFMode.c

--- NanoPi Code/Software/Modes/DummyDTMFMode.c
+++ NanoPi Code/Software/Modes/DummyDTMFMode.c
@@ -1,5 +1,42 @@
 
 void* DTMFCommandRelay(KeyPress* keyInput, RIG* radioDetails){
+    switch(keyInput->keyPressed){
+        case '0':
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+            PRINTFLEVEL1("Software: would be sending the dtmf digits here");
+            char dtmfValue[6] = {"DTMF", keyInput->keyPressed,'\0'}; 
+            sendSpeakerOutput(dtmfValue); 
+            break;
+        case '#':
+            PRINTFLEVEL1("Software: would be sending the dtmf digits here");
+            char dtmfValue[10] = {"DTMFPOUND\0"}; 
+            sendSpeakerOutput(dtmfValue); 
+            break;
+        case '*':
+            PRINTFLEVEL1("Software: would be sending the dtmf digits here");
+            char dtmfValue[13] = {"DTMFASTERISK\0"}; 
+            sendSpeakerOutput(dtmfValue); 
+            // char digit[2] = { keyInput->keyPressed, '\0' };
+            // int result = rig_send_dtmf(my_rig, RIG_VFO_CURR, digit);
+
+            // if (result != RIG_OK) {
+            //     PRINTFLEVEL1("Error sending DTMF digit: %s\n", rigerror(result));
+            // }
+            break;
+        default:
+        if(SIMULATEOUTPUT == 1){
+            PRINTFLEVEL1("got key of %c and nothing happens with that input\n",keyInput->keyPressed);
+        }
+        break;
+    }
     return NULL;
 }
 
