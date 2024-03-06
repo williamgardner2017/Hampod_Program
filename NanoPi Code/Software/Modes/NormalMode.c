d43f8a9d606e04e046878f68c22b28724c0796c7
williamgardner2017 <william.gardner2017@outlook.com>
2024-03-06 15:24:27 -0500

    Update NormalMode.c

--- NanoPi Code/Software/Modes/NormalMode.c
+++ NanoPi Code/Software/Modes/NormalMode.c
@@ -1,5 +1,5 @@
 
-freq_t* freq; 
+freq_t freq; 
 char vfoFreqValue[40];
 
 void* normalCommandRelay(KeyPress* keyInput, RIG* radioDetails){
@@ -10,13 +10,18 @@ void* normalCommandRelay(KeyPress* keyInput, RIG* radioDetails){
     
     case '1':
         if (!keyInput->isHold) {
-            rig_get_freq(radioDetails, RIG_VFO_CURR, &freq); 
-            sprintf(vfoFreqValue, "VFO A Frequency %.6f", freq);
+            // rig_get_freq(radioDetails, RIG_VFO_CURR, &freq); 
+            if (rig_get_freq(radioDetails, RIG_VFO_A, &freq) == RIG_OK) {
+                printf("Current frequency: %lf Hz\n", freq);
+            } else {
+                fprintf(stderr, "Error getting frequency.\n");
+            }
+            sprintf(vfoFreqValue, "VFO A frequency: %lf Hz\n", freq);
             sendSpeakerOutput(vfoFreqValue); 
         } else {
-            rig_get_freq(radioDetails, RIG_VFO_CURR, &freq); 
-            sprintf(vfoFreqValue, "VFO B Frequency %.6f", freq);
-            sendSpeakerOutput(vfoFreqValue); 
+            // rig_get_freq(radioDetails, RIG_VFO_CURR, &freq); 
+            // sprintf(vfoFreqValue, "VFO B Frequency %.6f", freq);
+            // sendSpeakerOutput(vfoFreqValue); 
         }
         break;
  
