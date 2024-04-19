8a5306eed7f1b1d0265969315abc6fdf59479fa1
williamgardner2017 <william.gardner2017@outlook.com>
2024-04-19 12:41:53 -0400

    Update NormalMode.c

--- NanoPi Code/Software/Modes/NormalMode.c
+++ NanoPi Code/Software/Modes/NormalMode.c
@@ -12,7 +12,10 @@ vfo_t general_vfo = RIG_VFO_CURR;
 int switchFuncMode(RIG* radioDetails) {
     printf("I run\n");
     int status; 
-    if (rig_get_func(radioDetails, &general_vfo, settingToChange, &status) == RIG_OK) {
+    retcode = rig_get_func(radioDetails, &general_vfo, settingToChange, &status);
+    if (retcode != RIG_OK) {
+        printf("rig_get_func RIT error: %s\n", rigerror(retcode));
+    } else {
         if (status == 0) {
             printf("Turn func on\n"); 
             return 1; 
